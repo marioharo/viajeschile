3927a8062b05e917d4a633a77f4870181deaa36d
marioharo <haromario2@gmail.com>
2024-03-31 17:58:30 -0500

    se agrega funcionalidad bootstrap js tooltip a iconos rr.ss en el footer

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+//smooth scrool de pdf
+// $(document).ready(function () {
+//     $("a").on('click', function (event) {
+//         if (this.hash !== "") {
+//             event.preventDefault(); var hash = this.hash;
+//             $('html, body').animate({
+//                 scrollTop: $(hash).offset().top
+//             }, 200, function () {
+//                 window.location.hash = hash;
+//             });
+//         }
+//     });
+// });
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
