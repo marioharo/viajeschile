fcc2a5ee6c1f2fd85bd86a166fa3a9e19e17fb8d
marioharo <haromario2@gmail.com>
2024-03-31 21:57:31 -0500

    funcionalidad jquery a navbar y cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-//smooth scrool de pdf
-// $(document).ready(function () {
-//     $("a").on('click', function (event) {
-//         if (this.hash !== "") {
-//             event.preventDefault(); var hash = this.hash;
-//             $('html, body').animate({
-//                 scrollTop: $(hash).offset().top
-//             }, 200, function () {
-//                 window.location.hash = hash;
-//             });
-//         }
-//     });
-// });
-
 // Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Cambio de color de fondo al navbar cuando se haga scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 20) {
+        $(".navbar").addClass("is-scrolling");
+    } else {
+        $(".navbar").removeClass("is-scrolling");
+    }
+});
+
+// Al hacer click en el título de las cards se esconde el texto
+$(".card-title").click(function () {
+    $(".card-text").toggle({
+      duration: 500,
+    }
+    )
+  });
